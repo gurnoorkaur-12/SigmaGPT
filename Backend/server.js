2dfbe5c43aa7601dcc7ccf4c322767756f97c529
GURNOOR KAUR <gurnoorkaur1226@gmail.com>
2026-06-14 01:42:33 +0530

    deployed frontend

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors(
-  { origin:"http://localhost:5173",
+  { origin:"https://sigmagpt-frontend-1odj.onrender.com",
     methods:['GET','POST','DELETE','PUT'],
     credentials:true
   }
